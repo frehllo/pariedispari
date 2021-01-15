6ae32c2732a50f5d0a8243f28dceab87e58a5b95
studenti <studenti@rosselli.loc>
2021-01-15 09:08:21 +0100

    Add project files.

---
+++ pariedispari/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace pariedispari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Thread thread = new Thread(StampaNumeri);
+                thread.Start();
+                StampaNumeri();
+            }
+            static void StampaNumeri()
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    int n = i;
+                    if (n % 2 == 0)
+                    {
+                        Console.WriteLine($"pari    {n}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"dispari {n}");
+                    }
+                }
+            }
+        }
+    }
+}
